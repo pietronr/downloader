2276fae2c45f8bb60da54e1a74d709f75913ebd8
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2024-07-15 20:16:10 -0300

    Classe estática Mp3Helper.

---
+++ Downloader/Helpers/Mp3Helper.cs
@@ -0,0 +1,19 @@
+﻿using NAudio.Lame;
+using NAudio.Wave;
+
+namespace Downloader.Helpers;
+
+public static class Mp3Helper
+{
+    /// <summary>
+    /// Convert um arquivo para mp3.
+    /// </summary>
+    /// <param name="inputPath">Caminho de entrada.</param>
+    /// <param name="outputPath">Caminho de saída.</param>
+    public static void ConvertToMp3(string inputPath, string outputPath)
+    {
+        using var reader = new MediaFoundationReader(inputPath);
+        using var writer = new LameMP3FileWriter(outputPath, reader.WaveFormat, LAMEPreset.STANDARD);
+        reader.CopyTo(writer);
+    }
+}
