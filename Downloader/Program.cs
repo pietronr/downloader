b56371f52c8a3dc89f9d30589b7ad298194193e8
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2024-07-15 20:16:20 -0300

    Novas dependências e ajustes de namespace.

--- Downloader/Program.cs
+++ Downloader/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Scripts.Classes.Youtube;
 
-using var download = new VideoDownloader();
+using var download = new YoutubeVideoDownloader();
 download.Initialize();
 await download.ExecuteDownload();
