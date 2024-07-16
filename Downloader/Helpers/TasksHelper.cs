c4188d389fc3226f44888185f240f9a6da8ee85c
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2024-07-16 20:33:50 -0300

    Método de execução paralela agora retorna itens que deram erro.

--- Downloader/Helpers/TasksHelper.cs
+++ Downloader/Helpers/TasksHelper.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+
 namespace Downloader.Helpers;
 
 public static class TasksHelper
@@ -9,14 +11,24 @@ public static class TasksHelper
     /// <param name="source">Lista de items.</param>
     /// <param name="action">Ação assíncrona a ser executada.</param>
     /// <param name="maxDegreeOfParallelism">Define o máximo de tasks paralelas.</param>
-    /// <returns></returns>
-    public static async Task ExecuteTasksWithLimitedConcurrency<T>(IEnumerable<T> source, Func<T, CancellationToken, Task> action, int maxDegreeOfParallelism = 4)
+    /// <returns>Um dicionário de items inválidos.</returns>
+    public static async Task<ConcurrentDictionary<T, string>> ExecuteTasksWithLimitedConcurrency<T>(IEnumerable<T> source, Func<T, CancellationToken, Task> action, int maxDegreeOfParallelism = 4) where T : class
     {
+        ConcurrentDictionary<T, string> invalidItems = [];
+
         await Parallel.ForEachAsync(source, new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism },
             async (item, token) =>
             {
-                // logic
-                await action(item, token);
+                try
+                {
+                    await action(item, token);
+                }
+                catch (Exception ex)
+                {
+                    invalidItems.TryAdd(item, ex.Message);
+                }
             });
+
+        return invalidItems;
     }
 }
