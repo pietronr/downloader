f1e852aa537a77bd993daa9050b9e998e2b1eb80
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2024-10-02 16:32:24 -0300

    feat: alterando DownloadedStream para DownloadedFilePath.

---
+++ Downloader/Classes/DownloadedFilePath.cs
@@ -0,0 +1,3 @@
+namespace Downloader.Classes;
+
+public readonly record struct DownloadedFilePath(string Mp4FilePath, string Mp3FilePath);
--- Downloader/Classes/DownloadedStream.cs
+++
@@ -1,8 +0,0 @@
-namespace Downloader.Classes;
-
-public class DownloadedStream(Stream stream, string fileTitle) 
-{
-    public Stream Stream { get; set; } = stream;
-
-    public string FileTitle { get; set; } = fileTitle;
-}
